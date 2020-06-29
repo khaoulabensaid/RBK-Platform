4f1b08f746fc41af24a0c8283bd33efca763f311
houssemguesmi <houssemguesmi14@gmail.com>
2020-06-29 14:04:54 +0100

    Let's Start

---
+++ database/index.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+mongoose.connect("mongodb://localhost:27017/RBK", { useNewUrlParser: true });
+let usersSchema = new Schema({
+  fullName: String,
+  email: String,
+  password: String,
+  role: String,
+  cohort: Number,
+});
+
+exports.RBK = mongoose.model("RBK", usersSchema);
+
+let chatRoomSchema = new Schema({
+  message: String,
+  messageSender: String,
+  messageSenderCohort: Number,
+  messageSenderRole: String,
+  createdAt: String,
+});
+
+exports.CHATROOM = mongoose.model("CHATROOM", chatRoomSchema);
