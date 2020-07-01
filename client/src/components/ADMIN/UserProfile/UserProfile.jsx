ca0f0162b396a17a759aedd2d5c19244d2346180
houssemguesmi <houssemguesmi14@gmail.com>
2020-07-01 17:06:52 +0100

    started the userNavbar

--- client/src/components/ADMIN/UserProfile/UserProfile.jsx
+++ client/src/components/ADMIN/UserProfile/UserProfile.jsx
@@ -13,6 +13,9 @@ class UserProfile extends React.Component {
     for (let i = 0; i < this.props.profiles.length; i++) {
       if (this.props.profiles[i].fullName === this.props.fullName) {
         const currentProfile = this.props.profiles[i];
+        if (currentProfile.Gender === "Female") {
+          this.setState({ image: "./female.jpg" });
+        }
         this.setState({ profile: currentProfile });
       }
     }
@@ -27,7 +30,7 @@ class UserProfile extends React.Component {
             </Col>
             <Col className="profile" sm="10">
               <p>FullName: {this.state.profile.fullName}</p>
-              <p>Gender: Male</p>
+              <p>Gender: {this.state.profile.Gender}</p>
               <p>Cohort: {this.state.profile.cohort} </p>
               <p> Role: {this.state.profile.role} </p>
               <p> E-mail: {this.state.profile.email} </p>
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,5 +3,8 @@ import ReactDOM from "react-dom";
 import DeleteCohortOrUsers from "./components/ADMIN/DeleteCohortOrUsers/DeleteCohortOrUsers.jsx";
 import App from "./components/App/App.jsx";
 import ChatRoom from "./components/ChatRoom/ChatRoom.jsx";
-import UserPorfile from "./components/ADMIN/UserProfile/UserProfile.jsx";
-ReactDOM.render(<App />, document.getElementById("app"));
+import UserProfileADMIN from "./components/ADMIN/UserProfile/UserProfile.jsx";
+import UserProfile from "./components/User/UserProfile/UserProfile.jsx";
+import UserNavbar from "./components/User/UserNavbar/UserNavbar.jsx";
+
+ReactDOM.render(<UserNavbar />, document.getElementById("app"));
