602d00e45468309728dd0a239e9ad39de39a8654
houssemguesmi <houssemguesmi14@gmail.com>
2020-07-02 14:47:24 +0100

    working on home

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,11 +5,11 @@ import Calendar from "./components/ADMIN/Calendar/Calendar.jsx";
 import DeleteCohortOrUsers from "./components/ADMIN/DeleteCohortOrUsers/DeleteCohortOrUsers.jsx";
 import App from "./components/App/App.jsx";
 import ChatRoom from "./components/ChatRoom/ChatRoom.jsx";
-import PinnedBoard from "./components/PinnedBoard/PinnedBoard.jsx"
+import PinnedBoard from "./components/PinnedBoard/PinnedBoard.jsx";
 import CreateAccount from "./components/ADMIN/CreateAccount/CreateAccount.jsx";
 import LoginTest from "./components/loginTest.jsx";
 import UserPorfileADMIN from "./components/ADMIN/UserProfile/UserProfile.jsx";
 import UserProfile from "./components/User/UserProfile/UserProfile.jsx";
 import UserNavbar from "./components/User/UserNavbar/UserNavbar.jsx";
 import Home from "./components/Home/Home.jsx";
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<Home />, document.getElementById("app"));
